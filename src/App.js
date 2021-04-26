066e5370ecc0832377e1edfee34e88da9e48c0d6
Alan Binu <65335042+AlanBinu007@users.noreply.github.com>
2021-04-26 09:19:57 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import Feed from './components/Body/Feed/Feed';
+import Sidebar from './components/Body/Sidebar/Sidebar';
+import Widgets from './components/Body/Widget/Widgets';
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // user is logged in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        }))
+      } else {
+        // use is logged out
+        dispatch(logout());
+      }
+    })
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app_body">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+        </div>
+      )}
+      
     </div>
   );
 }
